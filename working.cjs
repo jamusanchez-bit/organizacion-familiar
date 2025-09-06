426926b4714c26230c485252509f871b4f533bc0
jamusanchez-bit <jamusanchez@gmail.com>
2025-09-06 11:30:12 +0200

    Force redeploy v2

--- working.cjs
+++ working.cjs
@@ -873,5 +873,5 @@ function getAdminPage() {
 
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT} - v2`);
 });
